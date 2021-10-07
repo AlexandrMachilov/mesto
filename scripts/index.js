42b5a203d42c935b3041d75da29381873e8eb7ed
Alexandr Machilov <machilov00@mail.ru>
2021-10-07 22:20:56 +0300

    feat: add js

---
+++ scripts/index.js
@@ -0,0 +1,32 @@
+const popup = document.querySelector('.popup')
+const popupCloseButton = document.querySelector('.popup__button_action_close');
+const profileEditButton = document.querySelector('.button__action_edit');
+const popupForm = document.querySelector('.popup__form');
+const profileName = document.querySelector('.profile__name');
+const profileStatus = document.querySelector('.profile__status');
+const fieldName = document.querySelector('.popup__input_type_name');
+const fieldStatus = document.querySelector('.popup__input_type_status');
+
+
+function openPopup() {
+    fieldName.value = profileName.textContent; 
+    fieldStatus.value = profileStatus.textContent;
+    popup.classList.add('popup_isOpen');
+    console.log('open');
+    }
+
+function closePopup() {
+    popup.classList.remove('popup_isOpen');
+    console.log('close');
+} 
+
+function submitForm(event) {
+    event.preventDefault();
+    profileName.textContent = fieldName.value;
+    profileStatus.textContent = fieldStatus.value;
+    closePopup();
+    }
+
+profileEditButton.addEventListener('click', openPopup);
+popupCloseButton.addEventListener('click', closePopup);
+popupForm.addEventListener('submit', submitForm);
