707ecbe06820b6a03eef186486e3b32cd5ab6798
Alexandr Machilov <machilov00@mail.ru>
2021-10-18 21:12:45 +0300

    feat: add default fill

--- scripts/index.js
+++ scripts/index.js
@@ -6,6 +6,46 @@ const profileName = document.querySelector('.profile__name');
 const profileStatus = document.querySelector('.profile__status');
 const fieldName = document.querySelector('.popup__input_type_name');
 const fieldStatus = document.querySelector('.popup__input_type_status');
+const contentAddButton = document.querySelector('.profile__button_action_add');
+const elements = document.querySelector('.elements');
+const elementTemplate = document.querySelector('.element-template').content;
+
+const initialCards = [
+    {
+      name: 'Архыз',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    },
+    {
+      name: 'Челябинская область',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    },
+    {
+      name: 'Иваново',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    },
+    {
+      name: 'Камчатка',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    },
+    {
+      name: 'Холмогорский район',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    },
+    {
+      name: 'Байкал',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    }
+  ];
+
+initialCards.forEach(fillContent);
+
+function fillContent(card){
+    const element = elementTemplate.querySelector('.element').cloneNode(true);
+    element.querySelector('.element__image').src = card.link;
+    element.querySelector('.element__image').alt = card.name;
+    element.querySelector('.element__title').textContent = card.name;
+    elements.append(element);
+}
 
 
 function openPopup() {
@@ -35,3 +75,4 @@ profileEditButton.addEventListener('click', openPopup);
 popupCloseButton.addEventListener('click', closePopup);
 popup.addEventListener('mouseup', popupClickHandler);
 popupForm.addEventListener('submit', submitForm);
+contentAddButton.addEventListener('click', openPopup);
