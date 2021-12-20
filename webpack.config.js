7d8dc29d7a76ea3c3974f30c4238e4f32bf53499
Alexandr Machilov <machilov00@mail.ru>
2021-12-20 20:00:43 +0300

    Webpack set

---
+++ webpack.config.js
@@ -0,0 +1,55 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  entry: './src/scripts/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'),
+    },
+    compress: true,
+    port: 9000,
+    open: true
+  },
+  module: {
+    rules: [
+      {
+            test: /\.js$/,
+            exclude: /(node_modules)/,
+            use: {
+              loader: 'babel-loader'
+            }
+      },
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader,
+            {
+              loader: "css-loader",
+              options: {
+                importLoaders: 1,
+              },
+            },
+            "postcss-loader",
+          ],
+      },
+
+      {
+        test: /\.(png|svg|jpg|gif|woff(2)?|eot|ttf|otf)$/,
+        type: 'asset/resource',
+        generator: {
+            filename: 'assets/[hash][ext][query]'
+        }
+      },
+    ],
+  },
+  plugins: [new HtmlWebpackPlugin({
+      template: './src/index.html'
+  }),
+  new MiniCssExtractPlugin(),
+],
+};
